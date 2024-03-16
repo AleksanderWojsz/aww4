41f003020047e475b3cfd06181403ace040a7ca0
AleksanderWojsz <aw450252@students.mimuw.edu.pl>
2024-03-16 09:43:04 +0100

    Initial version

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        'moja': '10.052rem',
+      }
+    },
+  },
+  plugins: [],
+}
